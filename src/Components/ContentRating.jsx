e8f282a3785dd89829ebdb2e1ed1a5f654d7566d
Sergio El Kadi <selkadi@terra.com.br>
2024-07-26 18:25:55 -0400

    state trial

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,6 +5,10 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+        likes: 0,
+        dislikes: 0
+    }
   }
   render() {
     return (
