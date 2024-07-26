cecec13260952f24810137c8791b3a3670f22da5
Sergio El Kadi <selkadi@terra.com.br>
2024-07-26 18:23:44 -0400

    retorno

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,10 +5,6 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
-    this.state = {
-        likes: 0,
-        dislikes: 0
-    }
   }
   render() {
     return (
