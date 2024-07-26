3dc729bba811f4bb06be60bbc5f9a8b2ef4173ad
Sergio El Kadi <selkadi@terra.com.br>
2024-07-26 19:03:13 -0400

    until step 3.3

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -7,7 +7,17 @@ class ContentRating extends Component {
     super();
     this.state = {
         likes: 0,
-        dislikes: 0
+        dislikes: 0,
+        handleLike:() => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1
+        }));
+      },
+      handleDislike:() => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1
+        }));
+      }
     }
   }
   render() {
@@ -15,6 +25,14 @@ class ContentRating extends Component {
      <>
      <div className='content-rating'>
         <p>Do you like Genesis?</p>
+        <div className='rating-buttons'>
+            <button className="like-button">
+                Like ({this.state.likes})
+            </button>
+            <button className="dislike-button">
+                Dislike ({this.state.dislikes})
+            </button>
+        </div>
      </div>
      </>
     );
